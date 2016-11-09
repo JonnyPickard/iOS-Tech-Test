7e9bc6bc3ea9b69c8334a1253b968f2d162abeb7
Jonny Pickard <jonny@Jonnys-MacBook-Pro.local>
2016-11-09 16:05:43 +0000

    Pod Install
    Add Alamofire networking Pod
    Add SwiftyJSON JSON parsing Pod

---
+++ Podfile
@@ -0,0 +1,10 @@
+project './splittable.xcodeproj'
+
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target 'splittable' do
+    pod 'Alamofire'
+    pod 'SwiftyJSON'
+end
---
+++ splittable/APIRequestManager.swift
@@ -0,0 +1,15 @@
+//
+//  APIRequestManager.swift
+//  splittable
+//
+//  Created by Jonny Pickard on 09/11/2016.
+//  Copyright © 2016 Jonny Pickard. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class APIRequestManager: NSObject {
+    
+}
